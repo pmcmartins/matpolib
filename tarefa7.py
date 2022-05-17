0bd5a8b11925eeb235be92a185643654896d7028
pmcmartins <45401064+pmcmartins@users.noreply.github.com>
2022-05-17 23:10:34 +0100

    Initial commit

---
+++ tarefa7.py
@@ -0,0 +1,43 @@
+################## 1 ####################
+import numpy as np
+
+# criando um vetor
+#vet = np.array( [1,2,3,4,5] )
+
+#você pode imprimir usando a função print ou a função display
+#print(vet)
+#display(vet)
+
+############################# 2 ##################################
+
+#vet_seq1 = np.arange(8)
+
+#print(vet_seq1)
+
+
+#################### 4 ####################
+
+#for i in np.arange(2, 11):
+#    b = 0.0512
+ #   c = 'software'
+ #   print('%d %0.2f %s' % (i, b, c))
+
+#print('qualquer coisa')
+
+###################### 5 #########################
+
+# criando um vetor
+'''vetA = np.array(['a','b','c','d','e','f','g','h','i'])
+
+print('Três primeiros elementos de vetA: ')
+print( vetA[0:3] )
+
+print('\ntodos os valores após o Quinto elementos de vetA: ')
+print( vetA[5:] )
+
+print('\nOs três ultimos valores de vetA: ')
+print( vetA[-3:] )
+
+print('\nOs valores de vetA entre o 5 elemento até o penúltimo elemento: ')
+print( vetA[4:-2], 'ou', vetA[4:7] )'''
+
